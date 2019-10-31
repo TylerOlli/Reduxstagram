2c18ac44457317e684bdbe3c91a99545a4f3fbc3
TylerOlli <ollityler@gmail.com>
2019-10-30 20:51:13 -0400

    Created Redux Store

---
+++ client/Store.js
@@ -0,0 +1,21 @@
+import { createStore, compse } from "redux";
+import { syncHistoryWithStore } from "react-router-redux";
+import { browserHistory } from "react-router";
+
+// Import The Root Reducer
+import rootReducer from "./reducers/index";
+
+import comments from "./data/comments";
+import posts from "./data/posts";
+
+// Create An Object For Default Data
+const defaultState = {
+  posts,
+  comments
+};
+
+const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
+export default store;
