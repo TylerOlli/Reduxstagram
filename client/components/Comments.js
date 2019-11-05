a6a399dc4a3f75b848ad97e4d15ad080222dbacb
TylerOlli <ollityler@gmail.com>
2019-11-04 21:50:25 -0500

    Displayed Single Photo Component

---
+++ client/components/Comments.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Comments = React.createClass({
+  render() {
+    return <div className='comment'>I'm The Comments</div>;
+  }
+});
+
+export default Comments;
--- client/components/Single.js
+++ client/components/Single.js
@@ -1,9 +1,21 @@
 import React from "react";
-import { Link } from "react-router";
+import Photo from "./Photo";
+import Comments from "./Comments";
 
 const Single = React.createClass({
   render() {
-    return <div className='single-photo'>I'm the Single</div>;
+    // Index of Post
+    const i = this.props.posts.findIndex(
+      post => post.code === this.props.params.postId
+    );
+    const post = this.props.posts[i];
+    // Get Post
+    return (
+      <div className='single-photo'>
+        <Photo i={i} post={post} {...this.props} />
+        <Comments />
+      </div>
+    );
   }
 });
 
