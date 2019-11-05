30317f86782a8bdf56a031230a7cc45ee613ef50
TylerOlli <ollityler@gmail.com>
2019-11-04 19:18:59 -0500

    Setup Redux Actions and Reducers

---
+++ client/reducers/posts.js
@@ -0,0 +1,11 @@
+// Reducer Takes In
+
+// 1. The Action (Info About What Happened)
+// 2. Copy of Current State
+
+function posts(state = [], action) {
+  console.log(state, action);
+  return state;
+}
+
+export default posts;
