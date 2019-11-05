0c04e22c76f8fe234857ddce628ca4051f0c5653
TylerOlli <ollityler@gmail.com>
2019-11-04 21:36:21 -0500

    Updated State with Reducers

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -4,8 +4,19 @@
 // 2. Copy of Current State
 
 function posts(state = [], action) {
-  console.log(state, action);
-  return state;
+  switch (action.type) {
+    case "INCREMENT_LIKES":
+      // Return Updated State
+      console.log("Incrementing Likes");
+      const i = action.index;
+      return [
+        ...state.slice(0, i), // Before The One We Are Updating
+        { ...state[i], likes: state[i].likes + 1 },
+        ...state.slice(i + 1) // After The One We Are Updating
+      ];
+    default:
+      return state;
+  }
 }
 
 export default posts;
