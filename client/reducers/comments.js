ce3767eff2bd78bb9b3d2df3a34107200d43b961
TylerOlli <ollityler@gmail.com>
2019-11-04 22:55:17 -0500

    Redux Reducer Composition

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,4 +1,38 @@
+function postComments(state = [], action) {
+  switch (action.type) {
+    case "ADD_COMMENT":
+      // Return New State W/ New Comment
+      return [
+        ...state,
+        {
+          user: action.author,
+          text: action.comment
+        }
+      ];
+    case "REMOVE_COMMENT":
+      console.log("Removed A Comment");
+      // Need To Return New State W/O Deleted Comment
+      return [
+        // From Start To One That Gets Deleted
+        ...state.slice(0, action.i),
+        // After Deleted One, To End
+        ...state.slice(action.i + 1)
+      ];
+    default:
+      return state;
+  }
+  return state;
+}
+
 function comments(state = [], action) {
+  if (typeof action.postId !== "undefined") {
+    return {
+      // Take Current State
+      ...state,
+      // Overwrite This Post With New One
+      [action.postId]: postComments(state[action.postId], action)
+    };
+  }
   return state;
 }
 
