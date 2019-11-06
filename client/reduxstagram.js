2c5cba9c23c6d2355195b989189a0042b4dc9a73
TylerOlli <ollityler@gmail.com>
2019-11-05 23:28:14 -0500

    Setup Error Tracking and Insights with Sentry

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,7 +1,8 @@
 import React from "react";
+
 import { render } from "react-dom";
 
-// Import CSS
+// Import css
 import css from "./styles/style.styl";
 
 // Import Components
@@ -9,10 +10,15 @@ import App from "./components/App";
 import Single from "./components/Single";
 import PhotoGrid from "./components/PhotoGrid";
 
-// Import React Router Deps
+// import react router deps
 import { Router, Route, IndexRoute, browserHistory } from "react-router";
 import { Provider } from "react-redux";
 import store, { history } from "./store";
+
+import Raven from "raven-js";
+import { sentry_url, logException } from "./data/config";
+
+Raven.config(sentry_url).install();
 
 const router = (
   <Provider store={store}>
